9d2b3c9d4f99727574f7ec1b532f4828719e9768
arunraj-ar <mail.arunrajar@gmail.com>
2024-09-03 23:54:28 +0530

    card placement prediction

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,44 +1,81 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Card from "./Card"
 
 const Cards = ({data=[]}) => {
     const [cardsList , setCardsList] = useState(data);
 
+    const [targetIndex, setTargetIndex] = useState(null);
+    const [sourceIndex, setSourceIndex] = useState(null);
+
+    const getToIndex = (list=[], id='', x=0, y=0) => {
+        let targetIdx;
+        if(window.innerWidth < 1024) {
+            targetIdx = Math.floor(y/160)
+        } else {
+            targetIdx = Math.floor(x/160)
+        }
+        if(targetIdx<0){
+            targetIdx = 0;
+        } else if(targetIdx>list.length) {
+            targetIdx = list.length;
+        }
+
+        return targetIdx;
+    }
+
+    const getFromIndex = (list=[], id='') => {
+        let sourceIdx;
+        list.filter((item, index) => {
+            if(item.id === id) {
+                sourceIdx = index;
+                return true;
+            }
+            return false;
+        })
+        return sourceIdx;
+    }
+
+
+
     const handleDrag = (e) => {
         const xDrop = e.clientX;
         const yDrop = e.clientY;
         const card = e.target.id;
-        let targetIdx;
-        if(window.innerWidth < 1024) {
-            targetIdx = Math.floor(yDrop/160)
-        } else {
-            targetIdx = Math.floor(xDrop/160)
-        }
-        if(targetIdx<0){
-            targetIdx = 0;
-        } else if(targetIdx>cardsList.length) {
-            targetIdx = cardsList.length;
-        }
+        
+        const toIndex = getToIndex(cardsList, card, xDrop, yDrop)
 
         setCardsList((prev) => {
-            let fromIndex = 0;
             const next = [...prev];
-            next.filter((item, index) => {
-                if(item.id === card) {
-                    fromIndex = index;
-                    return true;
-                }
-                return false;
-            })
+            const fromIndex = getFromIndex(next, card)
 
             const [item] = next.splice(fromIndex, 1);
-            next.splice(targetIdx, 0, item);
+            next.splice(toIndex, 0, item);
             return [...next]
         })
     }
 
+    const handleDragging = (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        const card = e.target.id;
+        const toIndex = getToIndex(cardsList, card, x, y)
+        const fromIndex = getFromIndex(cardsList, card);
+        setTargetIndex(toIndex);
+        setSourceIndex(fromIndex);
+    }
+
+    useEffect(()=> {
+        setCardsList((prev) => {
+            const next = [...prev];
+
+            const [item] = next.splice(sourceIndex, 1);
+            next.splice(targetIndex, 0, item);
+            return [...next]
+        })
+    },[targetIndex, sourceIndex])
+
     return (
-        <div onDragEnd={handleDrag} className="bg-slate-600 flex lg:flex-row p-20 flex-col  min-h-dvh lg:min-h-0.5 lg:min-w-full">
+        <div onDragOver={handleDragging} onDragEnd={handleDrag} className="bg-slate-600 flex lg:flex-row p-20 flex-col  min-h-dvh lg:min-h-0.5 lg:min-w-full">
             {cardsList.map((card) => <Card key={card.id} {...card} />)}
         </div>
     )
